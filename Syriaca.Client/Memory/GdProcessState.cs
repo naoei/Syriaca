f4246fe966778aa6803a9969ccf70c961a6c75fd
naoei <altenyt@gmail.com>
2021-08-26 13:59:10 -0400

    Make GdReader independent from GdProcessState

--- Syriaca.Client/Memory/GdProcessState.cs
+++ Syriaca.Client/Memory/GdProcessState.cs
@@ -1,11 +1,27 @@
-﻿using Syriaca.Client.Information;
+﻿using System;
+using System.Diagnostics;
+using Syriaca.Client.Information;
 
 namespace Syriaca.Client.Memory
 {
     public class GdProcessState
     {
-        public GameScene Scene { get; set; }
+        public GameScene Scene { get; private set; }
+        public PlayerState PlayerState { get; }
 
-        public PlayerState PlayerState { get; } = new();
+        private readonly GdReader reader;
+
+        public GdProcessState(Process process)
+        {
+            Scene = GameScene.Unknown;
+            PlayerState = new PlayerState();
+
+            reader = new GdReader(process);
+        }
+
+        public Action<ValueChangedEvent<GameScene>> SceneChanged;
+
+        public (int user, int account) GetPlayerId()
+            => reader.ReadPlayerIds();
     }
 }
--- Syriaca.Client/Memory/GdReader.cs
+++ Syriaca.Client/Memory/GdReader.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using osu.Framework.Bindables;
 using Syriaca.Client.Information;
 
 namespace Syriaca.Client.Memory
@@ -11,31 +10,22 @@ namespace Syriaca.Client.Memory
         public const int GAME_MANAGER_ADDRESS = 0x3222D0;
         public const int ACCOUNT_MANAGER_ADDRESS = 0x3222D8;
         
-        //TODO: Eventually make our own bindable where we can manually call out if we have changes or not.
-        private readonly Bindable<GdProcessState> state;
         private readonly IntPtr gameManager;
         private readonly IntPtr accountManager;
         
-        public GdReader(Process process, Bindable<GdProcessState> processState)
+        public GdReader(Process process)
             : base(process)
         {
             gameManager = Memory.Read<IntPtr>((IntPtr) GAME_MANAGER_ADDRESS);
             accountManager = Memory.Read<IntPtr>((IntPtr) ACCOUNT_MANAGER_ADDRESS);
-            
-            state.BindTo(processState);
         }
 
         protected override AddressDictionary CreateAddressDictionary() => AddressDictionary.Parse(File.ReadAllText("Addresses.txt"));
 
-        public void UpdateScene()
-        {
-            state.Value.Scene = (GameScene) Read<int>("Current Scene", gameManager);
-        }
+        public GameScene ReadCurrentScene() 
+            => (GameScene) Read<int>("Current Scene", gameManager);
 
-        public void ReadPlayerIds()
-        {
-            state.Value.PlayerState.UserId = Read<int>("User ID", gameManager);
-            state.Value.PlayerState.AccountId = Read<int>("Account ID", accountManager);
-        }
+        public (int user, int account) ReadPlayerIds() 
+            => (Read<int>("User ID", gameManager), Read<int>("Account ID", accountManager));
     }
 }
